0554f7e8bc5fb9e69704741770b6fae61d723817
K <kbecker@primaryobjects.com>
2014-08-01 12:36:16 -0400

    Updated to Express 4.x. Updated to Twitter Bootstrap 3.2.0.

---
+++ README.md
@@ -0,0 +1,35 @@
+Node.js Bootstrap Starter Template
+===
+
+A quick and easy Node.js + Jade template project, using Twitter Bootstrap.
+
+##Usage
+- Clone repository.
+- Open a command prompt and type: npm install
+- Browse to http://localhost:3000
+
+##Demo:
+http://nodebootstrapper.herokuapp.com
+
+##Contents:
+
+layout.jade
+header.jade
+footer.jade
+index.jade
+
+##Editing Pages:
+
+Edit views/index.jade to add your content. The default contents include:
+
+```
+extends layout
+
+block content
+	div.container
+		h1 Program Name
+		p Hello World
+```
+
+##Author
+Kory Becker http://www.primaryobjects.com/kory-becker.aspx
--- app.js
+++ app.js
@@ -6,27 +6,27 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , favicon = require('serve-favicon')
+  , logger = require('morgan')
+  , methodOverride = require('method-override');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-    app.locals.pretty = true;
-});
+if (app.get('env') == 'development') {
+	app.locals.pretty = true;
+}
 
 app.get('/', routes.index);
 
